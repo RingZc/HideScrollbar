9f08f2868d87d0d079450bb27ebdc3df196530d5
RingZc <2928778236@qq.com>
2023-01-01 14:19:08 +0800

    Upload HideScrollbar

---
+++ index.js
@@ -0,0 +1,17 @@
+// 非常水的插件
+
+plugin.onLoad(async () => {
+    const cssText = `
+        *::-webkit-scrollbar,
+        *::-webkit-scrollbar-button,
+        *::-webkit-scrollbar-corner,
+        *::-webkit-scrollbar-thumb,
+        *::-webkit-scrollbar-track,
+        *::-webkit-scrollbar-track-piece {
+            background-color: transparent !important;
+        }
+    `;
+    const styleTag = document.createElement("style");
+    style.innerText = cssText;
+    document.head.appendChild(styleTag);
+});
